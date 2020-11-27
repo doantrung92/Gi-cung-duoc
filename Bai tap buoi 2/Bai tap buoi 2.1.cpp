4c03eddca3276870048de8631970921820564e1a
doantrung92 <74921651+doantrung92@users.noreply.github.com>
2020-11-27 16:34:02 +0700

    nop bai tap buoi 2

---
+++ Bai tap buoi 2/Bai tap buoi 2.1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+int a;
+printf ("Nhap a:");
+scanf ("%d", &a);
+if (a>=2 && a <=7){
+	printf ("Day la thu %d",a);
+	}else{
+		}
+if (a==8){
+	printf ("Day la chu nhat");
+	}else{
+	}
+if (a < 2 || a >8){
+	printf ("Khong phai ngay trong tuan", a);
+	}
+	return 0;
+	}
+	
---
+++ Bai tap buoi 2/Bai tap buoi 2.2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+int a,b;
+
+printf ("Nhap a=");
+scanf ("%d", &a);
+
+printf ("Nhap b=");
+scanf ("%d", &b);
+
+if (a>=b){
+	printf ("a/b=%d", a/b);
+	}else{
+		printf ("a*b=%d", a*b);
+		}
+	}
